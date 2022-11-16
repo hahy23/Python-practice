137c44ce016a580aa9e9001ad6c1a5457c537e78
hahy23 <hahy23@naver.com>
2022-11-16 13:25:08 +0900

    코딩연습 4 가변인수 함수 생성

--- Python 2.py
+++ Python 2.py
@@ -1,3 +1,5 @@
+
+'''
 a_list = [1,2,3,4,5,6]
 
 result = []
@@ -5,3 +7,12 @@ result = []
 for i in range(len(a_list)):
   result.append(a_list[i])
 print(result)
+'''
+
+def tuple_list(*args):
+  print(args)
+
+print(tuple_list(1,2,3,4))
+
+
+
